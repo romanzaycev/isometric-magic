948b221fdf29ab2c4943aa648967747d118719ca
Roman Zaycev <box@romanzaycev.ru>
2021-10-10 15:41:36 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace isometric_magic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
